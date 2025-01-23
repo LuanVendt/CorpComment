2ab837ff4d9f0b9b4b45c40da50b66f0f8c0e0f5
Luan Vendt <luan.vendt@bitize.com.br>
2025-01-23 10:27:03 -0300

    feat: implement feedback form with character limit and loading feedback items

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,28 +1,37 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
 
 export default function FeedbackList() {
+  const [feedBackItems, setFeedBackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchFeedbackItems() {
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+        const feedbackItems = await response.json();
+
+        setFeedBackItems(feedbackItems.feedbacks);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching feedback items", error);
+      }
+    }
+
+    fetchFeedbackItems();
+  }, []);
+
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>593</span>
-        </button>
+      {isLoading ? <Spinner /> : null}
 
-        <div>
-          <p>B</p>
-        </div>
-
-        <div>
-          <p>ByteGrad</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
-            laudantium fugiat perferendis rem cupiditate illo.
-          </p>
-        </div>
-
-        <p>4d</p>
-      </li>
+      {feedBackItems.map((feedbackItem) => (
+        <FeedbackItem feedbackItem={feedbackItem} />
+      ))}
     </ol>
   );
 }
